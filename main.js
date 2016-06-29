7a890e21771faa7824d9ea5a533372546c276716
Bipol Alam <balam@ispatechnology.com>
2016-06-29 16:24:58 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// main.js
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+class YouShould extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+    }
+
+    render() {
+        return ( <div>{this.props.date.toString()} 
+         {this.props.location ? this.props.location.coords.latitude : ''}</div> );
+    }
+}
+
+// grabs some information to render some components
+class InfoBox extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            pos: null
+        }
+    }
+
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            self = this;
+            navigator.geolocation.getCurrentPosition(function(pos) {
+                console.log(pos);
+                self.setState({ pos: pos });
+            });
+        } else {
+            self.setState({ pos: false });
+        }
+    }
+
+    render() {
+        return <YouShould location={this.state.pos} date={new Date()}/>
+    }
+}
+
+ReactDOM.render(
+  <InfoBox />, 
+  document.getElementById('react-root')
+);
+
+
---
+++ youshould/lib/router.ex
@@ -0,0 +1,47 @@
+defmodule Youshould.Router do
+    use Plug.Router
+    
+    plug :match
+    plug :dispatch
+    
+    def start_link do
+        {:ok, _} = Plug.Adapters.Cowboy.http(Youshould.Router, [])
+    end
+
+    # check the parameters to ensure they are okay
+    def check_params(params) do
+        resp = {:ok, params}
+        if !Map.has_key?(params, "date") do
+            resp = {:error, "Response must have date, latitude, and longitude"}
+        end
+
+        if !Map.has_key?(params, "latitude") do
+            resp = {:error, "Response must have date, latitude, and longitude"}
+        end
+
+        if !Map.has_key?(params, "longitude") do
+            resp = {:error, "Response must have date, latitude, and longitude"}
+        end
+    end
+
+    # query params:
+    # date/time : date time local to the user 
+    # latitude : lat of user position
+    # longitude: long of user position 
+    get "/api/getEvents" do
+        conn = fetch_query_params(conn)
+        case check_params(conn.params) do 
+            {:ok, _} ->
+                conn 
+                |> send_resp(200, "Yippeeeeeee")
+            {:error, body} ->
+                conn 
+                |> send_resp(400, body) 
+        end
+    end
+
+    match _ do 
+        conn
+        |> send_resp(404, "Nothing here.")
+    end
+end

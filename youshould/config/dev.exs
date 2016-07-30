056f291c56f1fc2ec1f98a1a9bcd8ea6046b035f
Bipol Alam <bipolalam@gmail.com>
2016-07-29 20:08:27 -0400

    remove youshould and move to a new repo

--- youshould/config/dev.exs
+++
@@ -1,2 +0,0 @@
-use Mix.Config
-config :youshould, owm_api_key: "API_KEY_HERE"
--- youshould/test/youshould_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule YoushouldTest do
-  use ExUnit.Case
-  doctest Youshould
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
